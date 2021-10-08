830953548d90ec19007963ede8ddbf26d02c920f
Ali <alibaniasad1999@yahoo.com>
2021-10-08 18:58:27 +0330

    fix MATAB to MATLAB

--- HW/HW I/Code/Q2/Q2.m
+++ HW/HW I/Code/Q2/Q2.m
@@ -3,17 +3,17 @@ a = 1;
 K = 1;
 G = K/(s * (s^2 + s + a));
 nyquist(G);
-print('../../Figure/Q2/MATAB-Nyquist','-dpng','-r400');
+print('../../Figure/Q2/MATLAB-Nyquist','-dpng','-r400');
 % a is bigger than K
 a = 10;
 K = 1;
 G = K/(s * (s^2 + s + a));
 nyquist(G);
-print('../../Figure/Q2/MATAB-Nyquist_a_bigger','-dpng','-r400');
+print('../../Figure/Q2/MATLAB-Nyquist_a_bigger','-dpng','-r400');
 % K is bigger than a
 a = 1;
 K = 10;
 G = K/(s * (s^2 + s + a));
 nyquist(G);
-print('../../Figure/Q2/MATAB-Nyquist_K_bigger','-dpng','-r400');
+print('../../Figure/Q2/MATLAB-Nyquist_K_bigger','-dpng','-r400');
 
