02bc7301d2285ab93aa73ee14c629b8c8872d189
Ali <alibaniasad1999@yahoo.com>
2021-10-15 19:34:35 +0330

    add nyquist

--- HW/HW II/Code/Q1/b/Q1_b.m
+++ HW/HW II/Code/Q1/b/Q1_b.m
@@ -5,7 +5,7 @@ gamma_bar = 45 + 5; % degree
 K = 5.1300;
 % solve phase equation (omega_g)
 syms omega_g
-% phase equation
+phase equation
 eq1 = atan(omega_g / 0.5) - atan(omega_g / 1) - 3 * atan(omega_g / 1.5) ...
     - atan(omega_g / 2) == -130 / 180 * pi;
 % solve equation 
@@ -21,10 +21,14 @@ s = tf('s');
 G_c = K_c * (s + 1 / T) / (s + 1 / (beta * T));
 bode(G_c);
 print('../../../Figure/Q1/b/controller.png','-dpng','-r400');
+nyquist(G_c);
+print('../../../Figure/Q1/b/controller_nyquist.png','-dpng','-r400');
 G   = 50 * (s+ 0.5)/((s+1)*(s+1.5)^3*(s+2));
 G_1 = K * G;
 margin(G_c * G);
 print('../../../Figure/Q1/b/new_margin.png','-dpng','-r400');
+nyquist(G_c * G);
+print('../../../Figure/Q1/b/new_nyquist.png','-dpng','-r400');
 bode(G_1);
 hold;
 bode(G_c);
@@ -32,16 +36,35 @@ bode(G_c*G);
 legend('system', 'lead compensation', 'system with lead compensation');
 print('../../../Figure/Q1/b/all_in_one.png','-dpng','-r400');
 hold off;
+% all nyquist
+nyquist(G_1);
+hold;
+nyquist(G_c);
+nyquist(G_c*G);
+legend('system', 'lead compensation', 'system with lead compensation');
+print('../../../Figure/Q1/b/all_in_one_nyquist.png','-dpng','-r400');
+hold off;
 %%%% change assume %%%%
 T = 10 / omega_g_ans;
 G_c = K_c * (s + 1 / T) / (s + 1 / (beta * T));
 bode(G_c);
 print('../../../Figure/Q1/b/new_controller.png','-dpng','-r400');
+nyquist(G_c);
+print('../../../Figure/Q1/b/new_controller_nyquist.png','-dpng','-r400');
 margin(G_c * G);
 print('../../../Figure/Q1/b/new2_margin.png','-dpng','-r400');
+nyquist(G_c * G);
+print('../../../Figure/Q1/b/new2_nyquist.png','-dpng','-r400');
 bode(G_1);
 hold;
 bode(G_c);
 bode(G_c*G);
 legend('system', 'lead compensation', 'system with lead compensation');
 print('../../../Figure/Q1/b/new_all_in_one.png','-dpng','-r400');
+hold off;
+nyquist(G_1);
+hold;
+nyquist(G_c);
+nyquist(G_c*G);
+legend('system', 'lead compensation', 'system with lead compensation');
+print('../../../Figure/Q1/b/new_all_in_one_nyquist.png','-dpng','-r400');
