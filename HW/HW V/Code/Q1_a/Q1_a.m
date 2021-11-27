979a5dfa34bf7bc9795c71b33f22a9e03a896823
Ali <alibaniasad1999@yahoo.com>
2021-11-27 17:22:37 +0330

    add question part a

--- HW/HW V/Code/Q1_a/Q1_a.m
+++ HW/HW V/Code/Q1_a/Q1_a.m
@@ -1,2 +1,16 @@
 s = tf('s');
 G = (s + 1) / (s^2 - 2 * s + 4);
+rlocus(G);
+print('../../Figure/Q1/Q1_a/rlocus.png','-dpng','-r400');
+step(feedback(G, 1));
+print('../../Figure/Q1/Q1_a/feedback_step.png','-dpng','-r400');
+bode(feedback(G, 1));
+print('../../Figure/Q1/Q1_a/feedback_bode.png','-dpng','-r400');
+bode(G);
+print('../../Figure/Q1/Q1_a/openloop_bode.png','-dpng','-r400');
+s_function = 1 / (1 + G);
+bode(s_function);
+print('../../Figure/Q1/Q1_a/s_bode.png','-dpng','-r400');
+t_function = G / (1 + G);
+bode(t_function);
+print('../../Figure/Q1/Q1_a/t_bode.png','-dpng','-r400');
