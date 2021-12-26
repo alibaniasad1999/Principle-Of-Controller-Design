e7429fdedd0c4bc04ef58d1a75e6948e0cdda989
unknown <alibaniasad1999@yahoo.com>
2021-12-26 21:10:10 +0330

    add question 1

---
+++ HW/HW VII/Code/Q1/optpidfun_3.m
@@ -0,0 +1,15 @@
+function y=optpidfun_3(x)
+% OPTPIDFUN_3   An objective function for optimal PID controller tuning
+%   It describes the relationship between the PID controller parameters
+%   and the chosen performance index.
+
+%   The function is automatically created by PID Controller Optimizer.
+%   Date of creation 26-Dec-2021
+opt=simset('OutputVariables','y');
+assignin('base','Kp',x(1));
+assignin('base','Ki',x(2));
+assignin('base','Kd',x(3));
+try
+   [~,~,y_out]=sim('pidctrl_model',[0,10.000000],opt);
+catch, y_out=1e10; end
+y=y_out(end,1);
