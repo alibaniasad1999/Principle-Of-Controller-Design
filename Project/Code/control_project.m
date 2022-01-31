a656a819354e120fdd0685f4412c00ccbd3a920e
Ali <alibaniasad1999@yahoo.com>
2022-01-31 20:10:02 +0330

    clean code

--- Project/Code/control_project.m
+++ Project/Code/control_project.m
@@ -1,17 +1,17 @@
 clc
-clear all
+clear
 
-%system specifications
-m = 1;%ball weight (kg)
-d = 0.05;%gear rediuce (m)
-a = 0.02;%ball rediuce (m)
-L = 1;%bar length (m)
-g = 9.8;%m/s^2
+% system specifications %
+m = 1   ; % ball weight (kg)
+d = 0.05; % gear rediuce (m)
+a = 0.02; % ball rediuce (m)
+L = 1   ; % bar length (m)
+g = 9.8 ; % m/s^2
 
-%transfer functions
+% transfer functions
 s=tf('s');
-R_thetagear = m*g*d/(7/5*L*s^2);
-thetamotor_V = 0.0274/(0.003228*s^2+0.003508*s);
-thetagear_thetamotor = 1/5;%gearbox ratio
+R_theta_gear = m * g * d / (7 / 5 * L * s^2);
+theta_motor_V = 0.0274 / (0.003228 * s^2 + 0.003508 * s);
+theta_gear_thetamotor = 1/5; % gearbox ratio
 
-G_R_V = R_thetagear * thetagear_thetamotor * thetamotor_V
+G_R_V = R_theta_gear * theta_gear_thetamotor * theta_motor_V;
