dd1bc874efc6d156b15e92f1d271ec57304bcb06
Ali <alibaniasad1999@yahoo.com>
2022-01-31 20:13:27 +0330

    clean

--- Project/Code/Base/Base_TF.m
+++ Project/Code/Base/Base_TF.m
@@ -10,8 +10,8 @@ g = 9.8 ; % m/s^2
 
 % transfer functions
 s=tf('s');
-R_theta_gear = m * g * d / (7 / 5 * L * s^2);
+R_theta_gear  = m * g * d / (7 / 5 * L * s^2);
 theta_motor_V = 0.0274 / (0.003228 * s^2 + 0.003508 * s);
-theta_gear_thetamotor = 1/5; % gearbox ratio
+theta_gear_theta_motor = 1/5; % gearbox ratio
 
-G_R_V = R_theta_gear * theta_gear_thetamotor * theta_motor_V;
+G_R_V = R_theta_gear * theta_gear_theta_motor * theta_motor_V;
