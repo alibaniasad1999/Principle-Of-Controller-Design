7601e92b0a0891d5febdf29952cb8ec36d06b656
Ali <alibaniasad1999@yahoo.com>
2022-01-31 20:30:44 +0330

    add actuator base

--- Project/Code/Base/Base_TF.m
+++ Project/Code/Base/Base_TF.m
@@ -9,9 +9,9 @@ L = 1   ; % bar length (m)
 g = 9.8 ; % m/s^2
 
 % transfer functions
-s=tf('s');
-R_theta_gear  = m * g * d / (7 / 5 * L * s^2);
-theta_motor_V = 0.0274 / (0.003228 * s^2 + 0.003508 * s);
-theta_gear_theta_motor = 1/5; % gearbox ratio
-
-G_R_V = R_theta_gear * theta_gear_theta_motor * theta_motor_V;
+s = tf('s');
+R_theta    = m * g * d / (7 / 5 * L * s^2); % R / theta
+theta_V    = 0.0274 / (0.003228 * s^2 + 0.003508 * s); % theta / V
+gear_motor = 1/5; % gearbox ratio
+% final 
+G_R_V = R_theta * gear_motor * theta_V;
