fc01783e5898d2411ff90fc23d2abc24c5019b3b
reza <rezabadbiy111111@gmail.com>
2022-01-31 21:55:41 +0330

    reza

--- Project/Code/control_project.m
+++ Project/Code/control_project.m
@@ -11,7 +11,10 @@ g = 9.8;%m/s^2
 %transfer functions
 s=tf('s');
 R_thetagear = m*g*d/(7/5*L*s^2);
-thetamotor_V = 0.0274/(0.003228*s^2+0.003508*s);
+thetamotor_V = 0.0274/(0.003228*s^2+0.003508*s);%Inner loop system tf
 thetagear_thetamotor = 1/5;%gearbox ratio
+
+R_thetamotor = R_thetagear * thetagear_thetamotor;%outer loop system tf
+
 
 G_R_V = R_thetagear * thetagear_thetamotor * thetamotor_V
