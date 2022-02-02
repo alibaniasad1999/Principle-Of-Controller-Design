3af143855cb8db3a0f479feed60969da228212f2
Ali <alibaniasad1999@yahoo.com>
2022-02-02 20:17:22 +0330

    fix with gayesh reza

--- Project/Code/Base/Base_TF.m
+++ Project/Code/Base/Base_TF.m
@@ -14,6 +14,5 @@ R_theta    = m * g * d / (7 / 5 * L * s^2); % R / theta
 theta_motor_V    = 0.0274 / (0.003228 * s^2 + 0.003508 * s); % theta / V
 gear_ratio = 1/5; % gearbox ratio
 R_theta_motor = R_theta * gear_ratio;
-R_V = 0.5; % R2V
-% final 
-G_R_V = R_V * R_theta * gear_ratio * theta_motor_V;
+% final tf
+G_R_V =  theta_motor_V * gear_ratio * R_theta;
