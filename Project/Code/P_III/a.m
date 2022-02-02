0d8ecc0cde101367007b96f26ee747b2b8fe437c
Ali <alibaniasad1999@yahoo.com>
2022-02-02 23:32:05 +0330

    add FOTD

--- Project/Code/P_III/a.m
+++ Project/Code/P_III/a.m
@@ -7,20 +7,23 @@ G_new_OpenLoop = feedback(C_stabilizer * G_R_V, 1);
 
 [K, L, T] = get_fod(G_new_OpenLoop); % frequesy
 G_freq = K * exp(-L * s) / (T * s + 1);
-% [K, L, T] = get_fod(G_new_OpenLoop, 1); % transfer function
-% G_tran = K * exp(-L * s) / (T * s + 1);
+[K, ~, T] = get_fod(G_new_OpenLoop, 1); % transfer function
+L = 0; % L is very small and function output is negative
+G_tran = K * exp(-L * s) / (T * s + 1);
 G_opt = opt_app(G_new_OpenLoop, 0, 1, 1); % optimization
 [K, L, T] = get_fod(G_opt); 
 G_opt = K * exp(-L * s) / (T * s + 1);
 
-step(G_new_OpenLoop);
-hold;
-step(G_freq);
-% step(G_tran);
-step(G_opt) ;
+t = 0:0.001:10; % time for step
+step(G_new_OpenLoop, t);
+hold on;
+step(G_freq, t);
+step(G_tran, t);
+step(G_opt, t) ;
+hold off;
 legend('system', 'frequecy FOTD', 'tranferfunction FOTD',...
     'optimization FOTD');
-% print('../../Figure/Q1/a/FOTD.png','-dpng','-r400');
-% Cost_freg = sum(abs(step(G)- step(G_freq)));
-% Cost_tran = sum(abs(step(G)- step(G_tran)));
-% Cost_opt  = sum(abs(step(G)- step(G_opt )));
+print('../../Figure/P_III/FOTD.png','-dpng','-r400');
+Cost_freg = sum(abs(step(G_new_OpenLoop, t)- step(G_freq, t)));
+Cost_tran = sum(abs(step(G_new_OpenLoop, t)- step(G_tran, t)));
+Cost_opt  = sum(abs(step(G_new_OpenLoop, t)- step(G_opt, t)));
